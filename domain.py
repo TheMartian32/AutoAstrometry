c66040e5b3b06b4277fc7c3bb40bcb36e93da669
Justin Buan <justin_b23@icloud.com>
2020-08-26 21:45:52 -0700

    Added functionality to look up target in the SIMBAD search portal.

--- domain.py
+++ domain.py
@@ -4,6 +4,7 @@ from rich import print
 import os
 import webbrowser
 import time
+from selenium import webdriver
 
 
 def ask_for(prompt, error_msg=None, _type=None):
@@ -84,6 +85,30 @@ def upload():
         #! If you are using this script please sign up for an account,
         #! then click on your images, then copy that URL and paste it here.
         webbrowser.open('http://nova.astrometry.net/users/20995')
+
+        print(
+            '\nWould you like to be [bold]redirected[/bold] to the [blue]SIMBAD search portal[/blue]?')
+        redirect_simbad = ask_for('(Y/N): ').lower()
+
+        if redirect_simbad == 'y':
+            # * Getting name of target to enter into SIMBAD search portal
+            print(
+                '\nWhat is the [bold]name[/bold] of your [blue]target[/blue]?')
+            target_name = ask_for('\n: ')
+
+            # * Opening SIMBAD URL.
+            browser = webdriver.Safari()
+            browser.get('http://simbad.u-strasbg.fr/simbad/sim-fbasic')
+            python_button = browser.find_element_by_xpath(
+                "/html/body/div[3]/div/form/table/tbody/tr[1]/td[2]/input")
+            python_button.send_keys(f'{target_name}')
+
+            python_button = browser.find_element_by_xpath(
+                "/html/body/div[3]/div/form/table/tbody/tr[3]/td[2]/input[1]")
+            python_button.click()
+
+        elif redirect_simbad == 'n':
+            print('Continuing...')
     else:
         #! Code to execute when solve fails
         print('\n[bold red]Failed[/bold red] to solve.')
